48dd24619f76bc00168c3c1131ab917ad74d0cdb
Radek Slachta <spool@seznam.cz>
2020-10-07 21:41:06 +0200

    add

--- Basic_Course/first.py
+++ Basic_Course/first.py
@@ -1,3 +1,27 @@
-name = input("What is your name, please? ")
-print("Hello, " + name  + "!")
+#name = input("What is your name, please? ")
+#print("Hello, " + name  + "!")
+
+john_doe = ['John Doe', 40]
+radek_sla = ['Radek Slachta', 44]
+database = [john_doe, radek_sla]
+
+test = "hello"
+
+print(test[-1])
+
+print(john_doe)
+
+print(database)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+copy_numbers_variable = numbers
+slicing_numbers = numbers[:]
+
+numbers.append(3)
+
+
+print(copy_numbers_variable)
+print(slicing_numbers)
+
+
 input("Press <enter>")
